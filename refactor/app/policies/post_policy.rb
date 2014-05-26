9c6738c1995f5db8c9da3f9d26f6a598b7e5df5e
Tony Chung <anthonyuchung@gmail.com>
2014-05-26 11:05:58 -0700

    Add authorization with pundit. Pre-adding role to user model with devise.

---
+++ refactor/app/policies/post_policy.rb
@@ -0,0 +1,28 @@
+class PostPolicy < ApplicationPolicy
+  def initialize (user, post)
+    @user = user
+    @post = post
+  end
+
+  def create?
+    @user.present? && @user.author? || @user.mod?
+  end
+
+  def update?
+    @user.present? && (@user.mod? || @user.author? && @user == @post.author)
+  end
+
+  def destroy?
+    @user.present? && @user.mod?
+  end
+
+  class Scope < Struct.new(:user, :scope)
+    def resolve
+      if user.present? && user.mod?
+        scope.all
+      else user.present? && user.author?
+        scope.where(:author => user)
+      end
+    end
+  end
+end
--- refactor/db/schema.rb
+++ refactor/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140523233005) do
+ActiveRecord::Schema.define(version: 20140526172642) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "author_id"
   end
 
   create_table "users", force: true do |t|
@@ -33,6 +34,7 @@ ActiveRecord::Schema.define(version: 20140523233005) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
