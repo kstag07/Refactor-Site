e27acaab75d09e8165eccc526ad092b9df87af01
Kevin Staiger <kstag07@yahoo.com>
2014-05-26 14:06:30 -0700

    adding comment feature to posts

--- refactor/db/schema.rb
+++ refactor/db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140526180746) do
+ActiveRecord::Schema.define(version: 20140526200510) do
+
+  create_table "comments", force: true do |t|
+    t.string   "author"
+    t.string   "author_url"
+    t.string   "user_ip"
+    t.string   "author_email"
+    t.string   "referrer"
+    t.text     "content"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "posts", force: true do |t|
     t.string   "title"
