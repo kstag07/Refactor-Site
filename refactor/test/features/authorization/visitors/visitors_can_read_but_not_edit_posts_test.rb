2ae547c3a69f9d4e0c1bdf06d64c32a3b8a82c3d
Steve Buckley <buckleys78@gmail.com>
2014-05-26 14:40:42 -0700

    before pull of default Role capability

--- refactor/test/features/authorization/visitors/visitors_can_read_but_not_edit_posts_test.rb
+++ refactor/test/features/authorization/visitors/visitors_can_read_but_not_edit_posts_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 feature "Vistor has certain capabilities" do
-  scenario "As a Visitor, I can see all posts." do
+  scenario "As a Visitor, I can see a post that is not mine." do
 
     #Given that I am not signed in,
 
@@ -12,16 +12,16 @@ feature "Vistor has certain capabilities" do
     page.must_have_content posts(:test_post).title
   end
 
-  scenario "As as Visitor, I cannot see a link to create new posts." do
+  scenario "As as Visitor, clicking Create Post takes me to Sign_in." do
     #Given that I am not signed in,
 
     #When I visit the posts page,
     visit posts_path
+save_and_open_page
+    click_on "New Post"
 
-
-    #Then I cannot see a link to create new posts,
-    page.wont_have_link "New Post"
-
+    #Then I will be redirected to the User Sign-in page.
+    assert_redirected_to user_session_path
   end
 
   # scenario "As a Visitor, I cannot type a URL and get access to new posts page" do
