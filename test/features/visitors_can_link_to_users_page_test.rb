20cdf23d943111379b3c48fd7937713ccd529ead
Kevin Staiger <kstag07@yahoo.com>
2014-05-29 15:46:59 -0700

    links

---
+++ test/features/visitors_can_link_to_users_page_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+feature do "As a visitor I want to be able to click on an author on the index page and go to their user page" do
+  scenario "author link takes you to user page" do
+    visit root_path
+    click_on posts(:post_by_mod).author
+    page.text.must_include posts(:post_by_mod).title
+    page.text.wont_include posts(:post_by_author).title
+  end
+end
