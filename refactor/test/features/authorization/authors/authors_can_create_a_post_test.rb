5d2cf298b7732d3d6c4b82948bc2973ce921f2f8
Steve Buckley <buckleys78@gmail.com>
2014-05-29 10:43:54 -0700

    updated testing requirements, all pass

--- refactor/test/features/authorization/authors/authors_can_create_a_post_test.rb
+++ refactor/test/features/authorization/authors/authors_can_create_a_post_test.rb
@@ -1,17 +1,17 @@
 require "test_helper"
 
-feature "As a user, I want to be the only editor of my posts" do
-  scenario "Only authors can edit their posts" do
+feature "As a user, I want to be able to contribute by making posts" do
+  scenario "An author can create a post." do
     sign_in(:author_user)
+    new_post_title = "a new post"
+    new_post_body = "body of new post"
+
     visit new_post_path
-    fill_in 'Title', with: 'Test author title'
-    fill_in 'Body', with: 'Test author body'
+    fill_in 'Title', with: new_post_title
+    fill_in 'Body', with: new_post_body
     click_on 'Create Post'
 
-    visit posts_path
-    page.text.must_include 'Edit'
-
-    click_on 'Sign out'
-    page.text.wont_include 'Edit'
+    page.text.must_include new_post_title
+    page.text.must_include new_post_body
   end
 end
