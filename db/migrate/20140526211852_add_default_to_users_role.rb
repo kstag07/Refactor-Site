00872264c03b555013ab4130bce5ad93e371f689
Tony Chung <anthonyuchung@gmail.com>
2014-08-28 15:10:05 -0700

    Remove binary and temporary files from Git

--- db/migrate/20140526211852_add_default_to_users_role.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultToUsersRole < ActiveRecord::Migration
-  def change
-    add_column :users, :role, :string, :default => 'author'
-  end
-end
--- db/migrate/20140527220615_add_author_id_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthorIdToComments < ActiveRecord::Migration
-  def change
-    add_column :comments, :author_id, :integer
-  end
-end
